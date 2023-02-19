482575bf5e1b069fc36b388392149f5d93113369
nathalieschro <Nathalie.schro@gmail.com>
2023-02-19 10:37:23 +0100

    Add project files.

---
+++ HemmeligProsjekt/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemmeligProsjekt
+{
+    public class Game
+    {
+        public bool CorrectGiftSelected { get; set; } = false;
+
+        public void PlayGame()
+        {
+            //Part 1 of game
+            IO.PrintWelcomeMessagePart1();
+
+            while (true)
+            {
+                IO.PrintGifts();
+                IO.PrintAskToRoll();
+                CheckTomasAnswer(IO.InputYesOrNo());
+                GivePrize(RollDice());
+                break;
+            }
+
+            //Part 2 of game
+            IO.PrintWelcomeMessagePart2();
+            while (CorrectGiftSelected == false)
+            {
+                IO.PrintAskToSelectGift();
+                GivePrize(IO.InputNumber());
+            }
+
+            IO.BirthdaySong();
+
+        }
+        public void CheckTomasAnswer(bool tomasAnswer)
+        {
+            if (tomasAnswer)
+            {
+                IO.PrintRollDice();
+            }
+            else
+            {
+                IO.PrintTomasTriesToAvoidToPlay();
+                IO.PrintRollDice();
+            }
+        }
+        public int RollDice()
+        {
+            //Dice will always return 0 because we can't afford the other gifts
+            return 0;
+        }
+
+        public void GivePrize(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    IO.PrintConsolationPrize();
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    IO.PrintExcuseForNotGivingGift(number);
+                    break;
+                case 6:
+                    IO.PrintWonFruitBasket();
+                    CorrectGiftSelected = true;
+                    break;
+                default:
+                    IO.PrintUnknownError();
+                    break;
+            }
+        }
+    }
+}
---
+++ HemmeligProsjekt/IO.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace HemmeligProsjekt
+{
+    public class IO
+    {
+        public static void PrintGifts()
+        {
+            List<string> Gifts = new List<string>()
+            {
+            "[1] Backstage tickets til Miley Cyrus",
+            "[2] Eksklusivt møte med Charlie Miller", 
+            "[3] Gratis tilgang til alle steam-spill",
+            "[4] Betalt 365 dagers ferie",
+            "[5] En all inclusive ferie til Bahamas for 4 personer",
+            "[6] Fruktkurv"
+            };
+
+            foreach (string gift in Gifts)
+            {
+                Console.WriteLine(gift);
+                Thread.Sleep(900);
+            }
+        }
+
+
+        public static void PrintWelcomeMessagePart1()
+        {
+            Console.Beep();
+            Console.WriteLine("Chief RubberDuck\n");
+            Thread.Sleep(1100);
+            Console.WriteLine("Du har bursdag i dag\n");
+            Thread.Sleep(1100);
+            Console.WriteLine("Velkommen til"); 
+            Thread.Sleep(1100);
+            Console.ForegroundColor= ConsoleColor.Magenta;
+            Console.WriteLine("ROLL-A-DICE-AND-WIN-A-BIRTHDAY-GIFT\n");
+            Console.Beep();
+            Console.ResetColor();
+            Thread.Sleep(1100);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("I dette spillet får du muligheten til å vinne en gave\n");
+            Console.ResetColor();
+            Thread.Sleep(900);
+        }
+
+        public static void PrintAskToRoll()
+        {
+            Console.WriteLine("\nVil du rrrrruuuuulllleeee terningen?");
+        }
+
+        public static void PrintAskToSelectGift()
+        {
+            Console.WriteLine("\nHvilken gave vil du ha?");
+        }
+
+        public static int InputNumber ()
+        {
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        public static bool InputYesOrNo()
+        {
+            Console.WriteLine("\nSkriv inn [j] for JA og [n] for NEIN");
+            char input = Console.ReadKey(intercept: true).KeyChar;
+            
+            if (input == 'j')
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void PrintUnknownError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Beep boop, unknown error, prøv igjen");
+            Console.ResetColor();
+        }
+
+        public static void PrintExcuseForNotGivingGift(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("\nÅnei, Hans kjøpte visst den siste tilgjengelige billetten. Prøv igjen.");
+                    break;
+                case 2:
+                    Console.WriteLine("\nCharlie hadde ikke tid til å møte deg. Prøv igjen");
+                    break;
+                case 3:
+                    Console.WriteLine("\nEhhh...Her var vi litt optimiske. Uhm. UTSOLGT sier Kristian. Prøv igjen");
+                    break;
+                case 4:
+                    Console.WriteLine("\nDu vil vel ikke ha ferie? Prøv igjen");
+                    break;
+                case 5:
+                    Console.WriteLine("\nFor fint vær i Bahamas i disse dager. Prøv igjen.");
+                    break;
+            }
+        }
+
+        public static void PrintWonFruitBasket()
+        {
+            Console.WriteLine("\nDu vant en fruktkurv. Hurra!");
+        }
+
+        public static void PrintRollDice()
+        {
+            Thread.Sleep(900);
+            Console.Clear();
+            Console.WriteLine(@"   ________");
+            Console.WriteLine(@"  /\  *  * \");
+            Console.WriteLine(@" /  \   *   \");
+            Console.WriteLine(@"/    \_______\");
+            Console.WriteLine(@"\ *  /       /");
+            Console.WriteLine(@" \  /  * *  /");
+            Console.WriteLine(@"  \/_______/");
+            Console.WriteLine(@"Terning triller");
+            Thread.Sleep(1000);
+
+            Console.WriteLine(@"   ________");
+            Console.WriteLine(@"  /\       \");
+            Console.WriteLine(@" /  \   ()  \");
+            Console.WriteLine(@"/    \_______\");
+            Console.WriteLine(@"\ () /       /");
+            Console.WriteLine(@" \  /   ()  /");
+            Console.WriteLine(@"  \/_______/");
+
+            Console.WriteLine("Terning rullet!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Terningen trillet 0...");
+            Thread.Sleep(4000);
+            Console.Clear();
+
+            Console.WriteLine(@"        .--'''''''''--.");
+            Console.WriteLine(@"     .'      .---.      '.");
+            Thread.Sleep(100);
+            Console.WriteLine(@"    /    .-----------.    \");
+            Console.WriteLine(@"   /        .-----.        \");
+            Thread.Sleep(100);
+            Console.WriteLine(@"   |       .-.   .-.       |");
+            Console.WriteLine(@"   |      /   \ /   \      |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"    \    | .-. | .-. |    /");
+            Console.WriteLine(@"     '-._| | | | | | |_.-'");
+            Thread.Sleep(100);
+            Console.WriteLine(@"         | '-' | '-' |");
+            Console.WriteLine(@"          \___/ \___/");
+            Thread.Sleep(100);
+            Console.WriteLine(@"       _.-'  /   \  `-._");
+            Console.WriteLine(@"     .' _.--|     |--._ '.");
+            Thread.Sleep(100);
+            Console.WriteLine(@"     ' _...-|     |-..._ '");
+            Console.WriteLine(@"            |     |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"            '.___.'");
+            Console.WriteLine(@"              | |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"             _| |_");
+            Console.WriteLine(@"            /\( )/\");
+            Thread.Sleep(100);
+            Console.WriteLine(@"           /  ` '  \");
+            Console.WriteLine(@"          | |     | |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"          '-'     '-'");
+            Console.WriteLine(@"          | |     | |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"          | |     | |");
+            Console.WriteLine(@"          | |-----| |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"       .`/  |     | |/`.");
+            Console.WriteLine(@"       |    |     |    |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"       '._.'| .-. |'._.'");
+            Console.WriteLine(@"             \ | /");
+            Thread.Sleep(100);
+            Console.WriteLine(@"             | | |");
+            Console.WriteLine(@"             | | |");
+            Thread.Sleep(100);
+            Console.WriteLine(@"             | | |");
+            Console.WriteLine(@"            /| | |\");
+            Thread.Sleep(100);
+            Console.WriteLine(@"          .'_| | |_`.");
+            Console.WriteLine(@"          `. | | | .'");
+            Thread.Sleep(100);
+            Console.WriteLine(@"       .    /  |  \    .");
+            Console.WriteLine(@"      /o`.-'  / \  `-.`o\");
+            Thread.Sleep(100);
+            Console.WriteLine(@"     /o  o\ .'   `. /o  o\");
+            Console.WriteLine(@"     `.___.'       `.___.'");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        public static void PrintTomasTriesToAvoidToPlay()
+        {
+            Console.WriteLine("Nei! Du må rulle terningen!\n");
+            Thread.Sleep(900);
+            Console.WriteLine("Når du skal være så vanskelig, ruller vi den for deg.");
+        }
+
+        public static void PrintConsolationPrize()
+        {
+            Console.WriteLine("Som takk for deltagelse får du en trøstepremie\n");
+            Thread.Sleep(1100);
+            Console.WriteLine("Du får den utlevert etter at spillet er ferdig\n");
+            Thread.Sleep(3200);
+        }
+
+        public static void PrintPrizeWonText()
+        {
+            Console.WriteLine("Gratulere, du er den eneste deltakeren som noensinne har valgt fruktkurven!\n");
+        }
+
+        public static void PrintWelcomeMessagePart2()
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Men spillet er ikke ferdig...\n");
+            Thread.Sleep(900);
+            Console.WriteLine("Du kan velge én av premiene fra første runde\n");
+            Thread.Sleep(1000);
+            PrintGifts();
+        }
+        public static void BirthdaySong()
+        {
+            Thread.Sleep(2000);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(330, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(2642, 500);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 250);
+            Thread.Sleep(125);
+            Console.Beep(352, 125);
+            Thread.Sleep(125);
+            Console.Beep(330, 500);
+            Thread.Sleep(125);
+            Console.Beep(297, 1000);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+        }
+    }
+}
